550f6891ca1a1fad031423098b3dadb91e9ffdf2
kurono7 <omar.aponte12@gmail.com>
2026-04-29 16:20:05 -0500

    Update main.tf
    path S3

--- main.tf
+++ main.tf
@@ -15,6 +15,8 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
+  s3_use_path_style           = true
+
   endpoints {
     s3  = "http://localhost:4566"
     ec2 = "http://localhost:4566"
